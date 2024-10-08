353c6b8101adc71d1610bf57743755ec601ec2ed
2dam <2dam@TartangaLH.eus>
2024-10-08 10:51:21 +0200

    creacion de excepciones y enum

---
+++ Library_SignUpSIgnIn/src/clases/Request.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Adrian Rocha
+ */
+public enum Request {
+    SING_UP,  // Registro de cuenta
+    SING_IN,  // Inicio de sesión
+    SING_OUT, // Cierre de sesión
+    CLOSE     // Apagar servidor
+}

f685fa69c94dacf093f4f626f0c78fff84dbdde3
2dam <2dam@TartangaLH.eus>
2024-10-10 08:31:59 +0200

    commit con las clases user y message

---
+++ Library_SignUpSIgnIn/src/clases/User.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author 2dam
+ */
+public class User {
+private String nombre;
+private Integer telefono;
+private String Correo;
+private String Ciudad;
+private String Contraseña;
+
+    /**
+     * Constructor vacio
+     */
+    public User() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getCiudad() {
+        return Ciudad;
+    }
+
+    public void setCiudad(String Ciudad) {
+        this.Ciudad = Ciudad;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
+       
+      
+      
+    
+}
